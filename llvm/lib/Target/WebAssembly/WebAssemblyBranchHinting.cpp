d794e4b7510fc3c0f739598aa988c2333ebe882a
Alon Zakai <azakai@google.com>
2025-06-11 10:20:41 -0700

    reading builds

--- llvm/lib/Target/WebAssembly/WebAssemblyBranchHinting.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblyBranchHinting.cpp
@@ -94,7 +94,7 @@ void WebAssemblyBranchHinting::visitBranchInst(BranchInst &I) {
   if (!TrueWeight || !FalseWeight)
     return;
 
-  std::cout << "waka " << TrueWeight->getZExtValue() << " : " << FalseWeight->getZExtValue() << '\n';
+  errs() << "waka " << TrueWeight->getZExtValue() << " : " << FalseWeight->getZExtValue() << '\n';
 
   // Generate metadata for wasm.
   // TODO
